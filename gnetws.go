375ef0c38911b7d77e27452955814e4a20699d6b
Pius-x <1105317070@qq.com>
2023-09-18 16:51:19 +0800

    Add files via upload

--- gnetws.go
+++ gnetws.go
@@ -1,7 +1,7 @@
 // Copyright 2022 gqzcl <gqzcl@qq.com>. All rights reserved.
 // Use of this source code is governed by a MIT style
 
-package main
+package gnetws
 
 import (
 	"context"
--- gnetws_test.go
+++ gnetws_test.go
@@ -1,4 +1,4 @@
-package main
+package gnetws
 
 import (
 	"fmt"
